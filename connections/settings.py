0cdd7de4e2824a50e3b4654a9c2207fbd13a28cd
kamda-cyrial <kamdacyrial@gmail.com>
2023-12-03 09:54:11 -0500

    adding localhost to allowed host

--- connections/settings.py
+++ connections/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["143.198.234.216"]
+ALLOWED_HOSTS = ["143.198.234.216", "127.0.0.1"]
 
 
 # Application definition
